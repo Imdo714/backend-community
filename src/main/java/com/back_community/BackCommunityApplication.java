3f906e8a319d7faa66fd3d50425a8b2bd59e2001
Imdo714 <gusehdla@naver.com>
2025-07-12 15:03:01 +0900

    “initial-commit

---
+++ src/main/java/com/back_community/BackCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.back_community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackCommunityApplication.class, args);
+	}
+
+}
