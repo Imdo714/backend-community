30a9a2fd3d3b51451a975ee72b6c87b132b322c8
Imdo714 <gusehdla@naver.com>
2025-07-14 17:13:14 +0900

    docs: 기상 게시물 리스트 문서화 작성

--- src/test/java/com/back_community/docs/wakeUpLog/WakeUpLogControllerDocsTest.java
+++ src/test/java/com/back_community/docs/wakeUpLog/WakeUpLogControllerDocsTest.java
@@ -2,15 +2,20 @@ package com.back_community.docs.wakeUpLog;
 
 
 import com.back_community.api.common.authentication.CustomUserPrincipal;
+import com.back_community.api.common.page.PageInfo;
 import com.back_community.api.wakeUpLog.board.controller.WakeUpLogController;
 import com.back_community.api.wakeUpLog.board.domain.dto.request.CreateWakeUpLogDto;
 import com.back_community.api.wakeUpLog.board.domain.dto.response.CreateWakeUpResponse;
+import com.back_community.api.wakeUpLog.board.domain.dto.response.WakeUpLogListResponse;
 import com.back_community.api.wakeUpLog.board.service.WakeUpLogService;
 import com.back_community.docs.RestDocsSupport;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.JsonFieldType;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
@@ -20,6 +25,7 @@ import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -68,6 +74,55 @@ public class WakeUpLogControllerDocsTest extends RestDocsSupport {
                                 fieldWithPath("data").type(JsonFieldType.OBJECT).description("데이터 객체"),
                                 fieldWithPath("data.wakeUpId").type(JsonFieldType.NUMBER).description("기상 기록 ID"),
                                 fieldWithPath("data.wakeUpStreak").type(JsonFieldType.NUMBER).description("기상 연속 횟수")
+                        )
+                ));
+    }
+
+    @DisplayName("기상 기록 리스트 API 문서화")
+    @Test
+    void wakeUpLogList() throws Exception {
+        // given
+        WakeUpLogListResponse.WakeUpList wakeUpList = WakeUpLogListResponse.WakeUpList.builder()
+                .wakeUpId(1L)
+                .title("기상 기록")
+                .createDate(LocalDateTime.now())
+                .isLike(true)
+                .build();
+
+        PageInfo pageInfo = new PageInfo(0, 5, 1, 5);
+
+        WakeUpLogListResponse response = WakeUpLogListResponse.builder()
+                .wakeUpLists(List.of(wakeUpList))
+                .pageable(pageInfo)
+                .build();
+
+        given(wakeUpLogService.getWakeUpLogList(any(), eq(0), eq(5))).willReturn(response);
+
+        // when & then
+        mockMvc.perform(get("/wake-up-log")
+                        .with(user(new CustomUserPrincipal(1L, "test@example.com")))
+                        .param("page", "0")
+                        .param("size", "5"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("wake-up-log-list",
+                        preprocessResponse(prettyPrint()),
+                        responseFields(
+                                fieldWithPath("code").type(JsonFieldType.NUMBER).description("응답 코드"),
+                                fieldWithPath("status").type(JsonFieldType.STRING).description("응답 상태"),
+                                fieldWithPath("message").type(JsonFieldType.STRING).description("응답 메시지"),
+                                fieldWithPath("data").type(JsonFieldType.OBJECT).description("응답 데이터"),
+
+                                fieldWithPath("data.wakeUpLists").type(JsonFieldType.ARRAY).description("기상 기록 리스트"),
+                                fieldWithPath("data.wakeUpLists[0].wakeUpId").type(JsonFieldType.NUMBER).description("기상 기록 ID"),
+                                fieldWithPath("data.wakeUpLists[0].title").type(JsonFieldType.STRING).description("기상 제목"),
+                                fieldWithPath("data.wakeUpLists[0].createDate").type(JsonFieldType.ARRAY).description("기록 생성일"),
+                                fieldWithPath("data.wakeUpLists[0].like").type(JsonFieldType.BOOLEAN).description("좋아요 여부"),
+
+                                fieldWithPath("data.pageable.totalElements").description("총 요소 수"),
+                                fieldWithPath("data.pageable.totalPages").description("총 페이지 수"),
+                                fieldWithPath("data.pageable.currentPage").description("현재 페이지"),
+                                fieldWithPath("data.pageable.size").description("페이지 사이즈")
                         )
                 ));
     }
