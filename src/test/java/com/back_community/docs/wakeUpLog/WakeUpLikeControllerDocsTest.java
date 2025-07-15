ce16161e606301850511a74728b4d1fd294ac387
Imdo714 <gusehdla@naver.com>
2025-07-15 17:34:17 +0900

    docs: 기상 게시물 좋아요 취소 API 문서화

--- src/test/java/com/back_community/docs/wakeUpLog/WakeUpLikeControllerDocsTest.java
+++ src/test/java/com/back_community/docs/wakeUpLog/WakeUpLikeControllerDocsTest.java
@@ -15,6 +15,7 @@ import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -52,5 +53,29 @@ public class WakeUpLikeControllerDocsTest extends RestDocsSupport {
                 ));
     }
 
+    @DisplayName("기상 게시물 댓글 삭제 API 문서화")
+    @Test
+    void wakeUpCommentDelete() throws Exception {
+        // given
+        Long logId = 1L;
+
+        // when then
+        mockMvc.perform(
+                        delete("/wake-up-log/{logId}/like", logId))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("wake-up-like-delete",
+                        preprocessResponse(prettyPrint()),
+                        pathParameters(
+                                parameterWithName("logId").description("기상 게시물 ID")
+                        ),
+                        responseFields(
+                                fieldWithPath("code").type(JsonFieldType.NUMBER).description("응답 코드"),
+                                fieldWithPath("status").type(JsonFieldType.STRING).description("응답 상태"),
+                                fieldWithPath("message").type(JsonFieldType.STRING).description("응답 메시지"),
+                                fieldWithPath("data").type(JsonFieldType.STRING).description("삭제 성공 메시지")
+                        )
+                ));
+    }
 
 }
