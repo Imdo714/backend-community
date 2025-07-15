041b6b5262bd23638c4af35061e75cb4c5d7cbd5
Imdo714 <gusehdla@naver.com>
2025-07-15 16:29:40 +0900

    docs: 기상 게시물 댓글 리스트 API 문서화

--- src/test/java/com/back_community/docs/wakeUpLog/WakeUpLogCommentControllerDocsTest.java
+++ src/test/java/com/back_community/docs/wakeUpLog/WakeUpLogCommentControllerDocsTest.java
@@ -1,7 +1,11 @@
 package com.back_community.docs.wakeUpLog;
 
+import com.back_community.api.common.authentication.CustomUserPrincipal;
+import com.back_community.api.common.page.PageInfo;
+import com.back_community.api.wakeUpLog.board.domain.dto.response.WakeUpLogListResponse;
 import com.back_community.api.wakeUpLog.comment.controller.WakeUpCommentController;
 import com.back_community.api.wakeUpLog.comment.domain.dto.request.CreateCommentDto;
+import com.back_community.api.wakeUpLog.comment.domain.dto.response.CommentListResponse;
 import com.back_community.api.wakeUpLog.comment.service.WakeUpCommentService;
 import com.back_community.docs.RestDocsSupport;
 import org.junit.jupiter.api.DisplayName;
@@ -9,12 +13,20 @@ import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.JsonFieldType;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -59,5 +71,58 @@ public class WakeUpLogCommentControllerDocsTest extends RestDocsSupport {
                 ));
     }
 
+    @DisplayName("기상 게시물 댓글 리스트 API 문서화")
+    @Test
+    void wakeUpCommentList() throws Exception {
+        // given
+        Long logId = 1L;
+        CommentListResponse.CommentList commentList = CommentListResponse.CommentList.builder()
+                .commentId(3L)
+                .imageUrl("http://img_Url")
+                .content("댓글 입니다.")
+                .createDate(LocalDateTime.now())
+                .build();
+
+        PageInfo pageInfo = new PageInfo(0, 5, 1, 5);
+
+        CommentListResponse response = CommentListResponse.builder()
+                .wakeUpCommentLists(List.of(commentList))
+                .pageable(pageInfo)
+                .build();
+
+        given(wakeUpCommentService.getCommentList(any(), eq(0), eq(5)))
+                .willReturn(response);
+
+        // when & then
+        mockMvc.perform(get("/wake-up-log/{logId}/comment", logId)
+                        .with(user(new CustomUserPrincipal(1L, "test@example.com")))
+                        .param("page", "0")
+                        .param("size", "5"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("wake-up-comment-list",
+                        preprocessResponse(prettyPrint()),
+                        pathParameters(
+                                parameterWithName("logId").description("기상 게시물 ID")
+                        ),
+                        responseFields(
+                                fieldWithPath("code").type(JsonFieldType.NUMBER).description("응답 코드"),
+                                fieldWithPath("status").type(JsonFieldType.STRING).description("응답 상태"),
+                                fieldWithPath("message").type(JsonFieldType.STRING).description("응답 메시지"),
+                                fieldWithPath("data").type(JsonFieldType.OBJECT).description("응답 데이터"),
+
+                                fieldWithPath("data.wakeUpCommentLists").type(JsonFieldType.ARRAY).description("기상 게시물 댓글 리스트"),
+                                fieldWithPath("data.wakeUpCommentLists[0].commentId").type(JsonFieldType.NUMBER).description("댓글 ID"),
+                                fieldWithPath("data.wakeUpCommentLists[0].imageUrl").type(JsonFieldType.STRING).description("댓글 작성자 프로필"),
+                                fieldWithPath("data.wakeUpCommentLists[0].content").type(JsonFieldType.STRING).description("댓글 내용"),
+                                fieldWithPath("data.wakeUpCommentLists[0].createDate").type(JsonFieldType.ARRAY).description("댓글 생성일"),
+
+                                fieldWithPath("data.pageable.totalElements").description("총 요소 수"),
+                                fieldWithPath("data.pageable.totalPages").description("총 페이지 수"),
+                                fieldWithPath("data.pageable.currentPage").description("현재 페이지"),
+                                fieldWithPath("data.pageable.size").description("페이지 사이즈")
+                        )
+                ));
+    }
 
 }
