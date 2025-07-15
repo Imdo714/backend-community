b0f25b8233d099af4c1115597ba9ab26340dd9ad
Imdo714 <gusehdla@naver.com>
2025-07-15 17:57:52 +0900

    docs: 로그인 성공 API 문서화

---
+++ src/test/java/com/back_community/docs/user/UserControllerDocsTest.java
@@ -0,0 +1,68 @@
+package com.back_community.docs.user;
+
+import com.back_community.api.user.controller.UserController;
+import com.back_community.api.user.domain.dto.request.LoginDto;
+import com.back_community.api.user.domain.dto.response.LoginResponse;
+import com.back_community.api.user.service.UserService;
+import com.back_community.docs.RestDocsSupport;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class UserControllerDocsTest extends RestDocsSupport {
+
+    private final UserService userService = mock(UserService.class);
+
+    @Override
+    protected Object initController() {
+        return new UserController(userService);
+    }
+
+    @DisplayName("로그인 API 문서화")
+    @Test
+    void login() throws Exception {
+        // given
+        LoginDto loginDto = new LoginDto("test@example.com", "password");
+
+        LoginResponse response = LoginResponse.builder().refreshToken("refreshToken 입니다.").build();
+
+        given(userService.loginAndGenerateToken(any(LoginDto.class)))
+                .willReturn(response);
+
+        // when then
+        mockMvc.perform(post("/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(loginDto))
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("user-login",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        requestFields(
+                                fieldWithPath("email").type(JsonFieldType.STRING).description("이메일"),
+                                fieldWithPath("password").type(JsonFieldType.STRING).description("비밀번호")
+                        ),
+                        responseFields(
+                                fieldWithPath("code").type(JsonFieldType.NUMBER).description("응답 코드"),
+                                fieldWithPath("status").type(JsonFieldType.STRING).description("응답 상태"),
+                                fieldWithPath("message").type(JsonFieldType.STRING).description("응답 메시지"),
+                                fieldWithPath("data.refreshToken").type(JsonFieldType.STRING).description("refreshToken")
+                        )
+                ));
+    }
+}
